18617c458f4ed357a4bf9fcf5de686abd1cd636a
Tuomas Martikainen <tuomas.martikainen@edu.karelia.fi>
2022-09-21 15:26:49 +0300

    Buttoneiden toteuttamista

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Helloworld 90000 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Background">
+      <div className="QuestionGroup">
+        <div className="RadioButton">
+          <input type="radio" value="Choice1" name="choice" /> Answer1
+        </div>
+        <div className="RadioButton">
+          <input type="radio" value="Choice2" name="choice" /> Answer2
+        </div>
+        <div className="RadioButton">
+          <input type="radio" value="Choice3" name="choice" /> Answer3
+        </div>
+      </div>
+      <div className="SubmitGroup">
+        <div className="SubmitButton">
+          <input type="button" value="Submit" />
+        </div>
+        <div className="SubmitButton">
+          <input type="button" value="Next" />
+        </div>
+      </div>
     </div>
   );
 }
